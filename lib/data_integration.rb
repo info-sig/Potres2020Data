1f9a0570b613a04df2c4bbfc3a6b3fb7ea76c984
Borna Novak <dosadnizub@gmail.com>
2021-01-07 14:10:02 +0100

    skeleton for the potres2020 thingy

---
+++ lib/data_integration.rb
@@ -0,0 +1,3 @@
+module DataIntegration
+  
+end
---
+++ lib/data_integration/open_it_potres_2020.rb
@@ -0,0 +1,19 @@
+module DataIntegration
+  class OpenItPotres2020
+
+    attr_accessor :opts
+
+    def initialize opts = {}
+      @opts = {}
+    end
+
+    def log_in
+      # TODO
+    end
+
+    def fetch_cases
+      # TODO
+    end
+
+  end
+end
