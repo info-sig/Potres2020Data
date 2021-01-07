c457a7317f7f451da2c5a606ac8f29ff869b8384
Borna Novak <dosadnizub@gmail.com>
2021-01-07 13:43:50 +0100

    added standard test libs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,24 @@
+require 'sidekiq/testing'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+raise "don't want to run tests if DATABASE_URL is set, I'm scared to drop a production database" if ENV['DATABASE_URL']
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
   # Add more helper methods to be used by all tests here...
+
+  def setup
+    TestableSecureRandom.make_random!
+    Sidekiq::Testing.fake!
+  end
+
+  def teardown
+    Sidekiq::Worker.clear_all
+    Timecop.return
+  end
+
 end
